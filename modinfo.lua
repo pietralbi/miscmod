16937818864696e100c55673ef3395d9ace39ea2
Albi <17704110+pietralbi@users.noreply.github.com>
2026-02-02 12:00:21 +0100

    bumped version

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,7 @@ name = "Miscellaneous tweaks"
 description = "Compilation of my script mods & tweaks, configurable"
 author = "Alberto Pietralunga"
 
-version = "1.3.0"
+version = "1.3.1"
 forumthread = ""
 
 api_version = 6
